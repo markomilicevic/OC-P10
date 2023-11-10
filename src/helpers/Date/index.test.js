bab3e1f1bc96c4222be419d631a59e5e19726bbf
Marko <marko@milicevic.fr>
2023-11-10 15:04:27 +0000

    Step 3: Fix month display

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,12 +1,28 @@
-import { sortListByDate } from './index'
+import { getMonth, sortListByDate } from "./index";
 
 describe("Date helper", () => {
   describe("When getMonth is called", () => {
-    it("the function return janvier for 2022-01-01 as date", () => {
-      // to implement
-    });
-    it("the function return juillet for 2022-07-08 as date", () => {
-      // to implement
+    describe.each([
+      ["2022-01-01", "janvier"],
+      ["2022-02-01", "février"],
+      ["2022-03-01", "mars"],
+      ["2022-04-01", "avril"],
+      ["2022-05-01", "mai"],
+      ["2022-06-01", "juin"],
+      ["2022-07-01", "juillet"],
+      ["2022-08-01", "août"],
+      ["2022-09-01", "septembre"],
+      ["2022-10-01", "octobre"],
+      ["2022-11-01", "novembre"],
+      ["2022-12-01", "décembre"],
+    ])("with %s", (date, expectedMonth) => {
+      it(`the function return ${expectedMonth}`, () => {
+        const input = new Date(`${date}T00:00:00.000Z`);
+
+        const output = getMonth(input);
+
+        expect(output).toBe(expectedMonth);
+      });
     });
   });
 
